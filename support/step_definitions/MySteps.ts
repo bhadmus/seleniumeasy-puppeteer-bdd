58f15157b7163563025e62b37c3eb289bd734c43
Ademola Bhadmus <bhadmusademola.1@gmail.com>
2021-08-23 21:37:59 +0100

    implemented Background Step Definition

--- support/step_definitions/MySteps.ts
+++ support/step_definitions/MySteps.ts
@@ -10,19 +10,30 @@ After(async function () {
     await page.close();
 })
 
-Given(/^I am on the test page$/, {timeout: 2 * 5000}, async function () {
+Given(/^I am on the test page$/, {timeout: 5 * 6000}, async function () {
     await page.open(ElementMapper.home);
 });
-Then(/^I close the popup$/, {timeout: 2 * 5000}, async function () {
+Then(/^I close the popup$/, async function () {
     await page.clickElement(ElementMapper.closePopup);
 });
-When(/^I click the "([^"]*)" option$/, async function () {
+When(/^I click the "([^"]*)" option$/, async function (type) {
+        if (type==="list box") {
+           await page.clickElement(ElementMapper.listBox);
+        }else if (type === "jquery list box") {
+            await page.clickElement(ElementMapper.jqListBox);
+            
+        }
+});
+Then(/^I should be able to see the "([^"]*)" on the list$/, async function (type) {
+    if (type === "jquery list box") {
+        await page.waitAction(ElementMapper.jqListBox);
+    }
 
 });
-Then(/^I should be able to see the "([^"]*)" on the list$/, async function () {
-
-});
-Then(/^The "([^"]*)" page should load successfully$/, async function () {
+Then(/^The "([^"]*)" page should load successfully$/, async function (type) {
+    if (type === "jquery list box"){
+        await page.waitAction(ElementMapper.addButton);
+    }
 
 });
 When(/^I select "([^"]*)" of the names$/, async function () {
